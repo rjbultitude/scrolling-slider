7f324ea8eab14fc9febd18b36ae9220d682f1cb7
Richard Bultitude <rbultitude@thisiszone.com>
2015-04-27 18:12:55 +0100

    namespace the main app

--- js/main.js
+++ js/main.js
@@ -11,267 +11,275 @@
 
 $(document).ready(function() {
 
-	//Interval  vars
-	var scrollerInvervalRight;
-	var scrollerInvervalLeft;
-	var firedRight = false;
-	var firedLeft = false;
+	//var scrollingSlider = scrollingSlider || {};
 
-	//Elements & position
-	var container = $('.carousel');
-	var mainContent = $('.content');
-	var currentScrollPos = container.scrollLeft();
-	var controls = $('.direction-control');
-	var leftControl = $('.direction-control-left');
-	var rightControl = $('.direction-control-right');
-	var rate = 0;
-	// var maxspeed = 1;
-	var frameRate = 10;
-	// var xPos = 0;
+	(function(scrollingSlider) {
+		if (scrollingSlider !== undefined) {
+			return false;
+		}
 
-	//List items
-	var list = $('.carousel-list');
-	var totalWidth = 0;
+		scrollingSlider = {};
+		//Interval  vars
+		scrollingSlider.scrollerInvervalRight;
+		scrollingSlider.scrollerInvervalLeft;
+		scrollingSlider.firedRight = false;
+		scrollingSlider.firedLeft = false;
 
-	//break points
-	var breakPoint = 480;
-	//var breakPointCheckMin = null || window.matchMedia('all and (min-width: ' + breakPoint + 'px)');
-	var breakPointCheckMax = null || window.matchMedia('all and (max-width: ' + breakPoint + 'px)');
+		//Elements & position
+		scrollingSlider.container = $('.carousel');
+		scrollingSlider.mainContent = $('.content');
+		scrollingSlider.currentScrollPos = scrollingSlider.container.scrollLeft();
+		scrollingSlider.controls = $('.direction-control');
+		scrollingSlider.leftControl = $('.direction-control-left');
+		scrollingSlider.rightControl = $('.direction-control-right');
+		scrollingSlider.rate = 0;
+		scrollingSlider.frameRate = 10;
 
-	function getListSize() {
-		//count modules
-		var listItemSizes = [];
-		$('li', list).each(function() {
-			var thisItem = $(this);
-			listItemSizes.push(thisItem.outerWidth());
-		});
-		for (var i = 0; i < listItemSizes.length; i++) {
-			totalWidth = totalWidth + listItemSizes[i];
-		};
-	}
+		//List items
+		scrollingSlider.list = $('.carousel-list');
+		scrollingSlider.totalWidth = 0;
 
-	function setListSize() {
-		list.width(totalWidth);
-	}
+		//break point
+		scrollingSlider.breakPoint = 480;
+		scrollingSlider.breakPointCheckMax = null || window.matchMedia('all and (max-width: ' + scrollingSlider.breakPoint + 'px)');
 
-	function hideControlsAtEnd() {
-		var containerWidth = container.width();
-		var listWidth = totalWidth;
-		var scrollArea = listWidth - containerWidth;
-		var scrollPos = container.scrollLeft();
+		function getListSize() {
+			//count modules
+			var listItemSizes = [];
+			$('li', scrollingSlider.list).each(function() {
+				var thisItem = $(this);
+				listItemSizes.push(thisItem.outerWidth());
+			});
+			for (var i = 0; i < listItemSizes.length; i++) {
+				scrollingSlider.totalWidth = scrollingSlider.totalWidth + listItemSizes[i];
+			};
+		}
 
-		if (containerWidth >= listWidth) {
-			leftControl.fadeOut();
-			rightControl.fadeOut();
-		} else {
-			//show/hide right
-			if (scrollPos === scrollArea) {
-				rightControl.fadeOut();
+		function setListSize() {
+			scrollingSlider.list.width(scrollingSlider.totalWidth);
+		}
+
+		function hideControlsAtEnd() {
+			var containerWidth = scrollingSlider.container.width();
+			var listWidth = scrollingSlider.totalWidth;
+			var scrollArea = listWidth - containerWidth;
+			var scrollPos = scrollingSlider.container.scrollLeft();
+
+			if (containerWidth >= listWidth) {
+				scrollingSlider.leftControl.fadeOut();
+				scrollingSlider.rightControl.fadeOut();
 			} else {
-				rightControl.fadeIn();
-			}
-			//show/hide left
-			if (scrollPos === 0) {
-				leftControl.fadeOut();
-			} else {
-				leftControl.fadeIn();
+				//show/hide right
+				if (scrollPos === scrollArea) {
+					scrollingSlider.rightControl.fadeOut();
+				} else {
+					scrollingSlider.rightControl.fadeIn();
+				}
+				//show/hide left
+				if (scrollPos === 0) {
+					scrollingSlider.leftControl.fadeOut();
+				} else {
+					scrollingSlider.leftControl.fadeIn();
+				}
 			}
 		}
-	}
 
-	container.scroll(function() {
-		hideControlsAtEnd();
-	});
-
-	//Create invervals for animation
-	function setIntervalRight() {
-		scrollerInvervalRight = setInterval(function() {
-			moveItRight();
-		}, frameRate);
-	}
-
-	function setIntervalLeft() {
-		scrollerInvervalLeft = setInterval(function() {
-			moveItLeft();
-		}, frameRate);
-	}
-
-	//Clear intervals at stops
-	function clearIntervalRight() {
-		clearInterval(scrollerInvervalRight);
-		scrollerInvervalRight = 0;
-	}
-
-	function clearIntervalLeft() {
-		clearInterval(scrollerInvervalLeft);
-		scrollerInvervalLeft = 0;
-	}
-
-	//Interaction functions
-	function assignMouseTouchEvents() {
-		$('.direction').on('click onLongPress', function(event) {
-			event.preventDefault();
+		scrollingSlider.container.scroll(function() {
+			hideControlsAtEnd();
 		});
 
-		rightControl.on('mouseenter touchstart', function(event) {
-			event.stopPropagation();
-			setIntervalRight();
-		});
+		//Create invervals for animation
+		function setIntervalRight() {
+			scrollerInvervalRight = setInterval(function() {
+				moveItRight();
+			}, scrollingSlider.frameRate);
+		}
 
-		rightControl.on('mouseleave touchend', function() {
+		function setIntervalLeft() {
+			scrollerInvervalLeft = setInterval(function() {
+				moveItLeft();
+			}, scrollingSlider.frameRate);
+		}
+
+		//Clear intervals at stops
+		function clearIntervalRight() {
+			clearInterval(scrollerInvervalRight);
+			scrollingSlider.scrollerInvervalRight = 0;
+		}
+
+		function clearIntervalLeft() {
+			clearInterval(scrollerInvervalLeft);
+			scrollingSlider.scrollerInvervalLeft = 0;
+		}
+
+		//Interaction functions
+		function assignMouseTouchEvents() {
+			$('.direction').on('click onLongPress', function(event) {
+				event.preventDefault();
+			});
+
+			scrollingSlider.rightControl.on('mouseenter touchstart', function(event) {
+				event.stopPropagation();
+				setIntervalRight();
+			});
+
+			scrollingSlider.rightControl.on('mouseleave touchend', function() {
+				clearIntervalRight();
+			});
+
+			scrollingSlider.leftControl.on('mouseenter touchstart', function(event) {
+				event.stopPropagation();
+				setIntervalLeft();
+			});
+
+			scrollingSlider.leftControl.on('mouseleave touchend', function() {
+				clearIntervalLeft();
+			});
+			$('.stop').on('click', function() {
+				clearAllIntervals();
+			});
+			//Remove mouse events on touch devices
+			unAssignMouseTouchEvents();
+		}
+
+		function unAssignMouseTouchEvents() {
+			if (Modernizr.touch) {
+				scrollingSlider.rightControl.off('mouseenter');
+				scrollingSlider.leftControl.off('mouseenter');
+			}
+		}
+
+		function clearAllIntervals() {
 			clearIntervalRight();
-		});
-
-		leftControl.on('mouseenter touchstart', function(event) {
-			event.stopPropagation();
-			setIntervalLeft();
-		});
-
-		leftControl.on('mouseleave touchend', function() {
 			clearIntervalLeft();
-		});
-		$('.stop').on('click', function() {
-			clearAllIntervals();
-		});
-		//Remove mouse events on touch devices
-		unAssignMouseTouchEvents();
-	}
-
-	function unAssignMouseTouchEvents() {
-		if (Modernizr.touch) {
-			rightControl.off('mouseenter');
-			leftControl.off('mouseenter');
 		}
-	}
 
-	function clearAllIntervals() {
-		clearIntervalRight();
-		clearIntervalLeft();
-	}
+		//Set scroll position(s)
+		function moveItRight() {
+			currentScrollPos = scrollingSlider.container.scrollLeft();
+			var _newScrollPos = 1 + Math.abs(rate);
+			_newScrollPos = currentScrollPos + _newScrollPos;
+			scrollingSlider.container.scrollLeft(_newScrollPos);
+		}
 
-	//Set scroll position(s)
-	function moveItRight() {
-		currentScrollPos = container.scrollLeft();
-		var _newScrollPos = 1 + Math.abs(rate);
-		_newScrollPos = currentScrollPos + _newScrollPos;
-		container.scrollLeft(_newScrollPos);
-	}
+		function moveItLeft() {
+			scrollingSlider.currentScrollPos = scrollingSlider.container.scrollLeft();
+			var _newScrollPos = 1 + Math.abs(rate);
+			_newScrollPos = scrollingSlider.currentScrollPos - _newScrollPos;
+			scrollingSlider.container.scrollLeft(_newScrollPos);
+		}
 
-	function moveItLeft() {
-		currentScrollPos = container.scrollLeft();
-		var _newScrollPos = 1 + Math.abs(rate);
-		_newScrollPos = currentScrollPos - _newScrollPos;
-		container.scrollLeft(_newScrollPos);
-	}
+		//Change the animation rate based on position of cursor
+		function directionMouseMove() {
+			scrollingSlider.controls.mousemove(function(e) {
+				var $this = $(this);
+				var left = $this.is('.direction-control-left');
+				var w = $this.width();
 
-	//Change the animation rate based on position of cursor
-	function directionMouseMove() {
-		controls.mousemove(function(e) {
-			var $this = $(this);
-			var left = $this.is('.direction-control-left');
-			var w = $this.width();
+				if (left) {
+					rate = ((w - e.pageX - $(this).offset().left + 1) / w) * 3.5;
+				} else {
+					rate = -((e.pageX - $(this).offset().left + 1) / w) * 3.5;
+				}
+			});
+		}
 
-			if (left) {
-				rate = ((w - e.pageX - $(this).offset().left + 1) / w) * 3.5;
+		//Keyboard controls
+		function assignKeyboardEvents() {
+			$(document).on('keydown', function(e) {
+				switch (e.keyCode) {
+					case 39:
+						if (firedRight === false) {
+							firedRight = true;
+							setIntervalRight();
+						}
+						break;
+					case 37:
+						if (firedLeft === false) {
+							firedLeft = true;
+							setIntervalLeft();
+						}
+						break;
+				}
+			});
+
+			$(document).on('keyup', function(e) {
+				switch (e.keyCode) {
+					case 39:
+						if (firedRight === true) {
+							clearIntervalRight();
+							firedRight = false;
+						}
+						break;
+					case 37:
+						if (firedLeft === true) {
+							clearIntervalLeft();
+							firedLeft = false;
+						}
+						break;
+				}
+			});
+		}
+
+		function killDirection() {
+			controls.css({
+				'display': 'none'
+			}).addClass('inactive');
+		}
+
+		function runDirection() {
+			scrollingSlider.controls.css({
+				'display': 'block'
+			}).removeClass('inactive');
+			hideControlsAtEnd();
+		}
+
+		//Show the controls when
+		//the mouse moves across the surface
+		function controlsMouseMove() {
+			function addMove() {
+				scrollingSlider.mainContent.addClass('move');
+				setTimeout(function() {
+					scrollingSlider.mainContent.removeClass('move');
+				}, 2000);
+			}
+			scrollingSlider.container.on('mousemove', $.throttle(2000, addMove));
+		}
+
+		//Viewport size logic
+		function initResize() {
+			var windowWidth = 0;
+			$(window).resize(function() {
+				checkViewPortSize();
+				windowWidth = $(window).width();
+			});
+			return windowWidth;
+		}
+
+		function checkViewPortSize() {
+			if (scrollingSlider.breakPointCheckMax.matches) {
+				//kill app
+				scrollingSlider.list.removeAttr('style');
+				killDirection();
 			} else {
-				rate = -((e.pageX - $(this).offset().left + 1) / w) * 3.5;
+				//re-run app
+				setListSize();
+				runDirection();
 			}
-		});
-	}
-
-	//Keyboard controls
-	function assignKeyboardEvents() {
-		$(document).on('keydown', function(e) {
-			switch (e.keyCode) {
-				case 39:
-					if (firedRight === false) {
-						firedRight = true;
-						setIntervalRight();
-					}
-					break;
-				case 37:
-					if (firedLeft === false) {
-						firedLeft = true;
-						setIntervalLeft();
-					}
-					break;
-			}
-		});
-
-		$(document).on('keyup', function(e) {
-			switch (e.keyCode) {
-				case 39:
-					if (firedRight === true) {
-						clearIntervalRight();
-						firedRight = false;
-					}
-					break;
-				case 37:
-					if (firedLeft === true) {
-						clearIntervalLeft();
-						firedLeft = false;
-					}
-					break;
-			}
-		});
-	}
-
-	function killDirection() {
-		controls.css({
-			'display': 'none'
-		}).addClass('inactive');
-	}
-
-	function runDirection() {
-		controls.css({
-			'display': 'block'
-		}).removeClass('inactive');
-		hideControlsAtEnd();
-	}
-
-	//Show the controls when
-	//the mouse moves across the surface
-	function controlsMouseMove() {
-		function addMove() {
-			mainContent.addClass('move');
-			setTimeout(function() {
-				mainContent.removeClass('move');
-			}, 2000);
 		}
-		container.on('mousemove', $.throttle(2000, addMove));
-	}
 
-	//Viewport size logic
-	function initResize() {
-		var windowWidth = 0;
-		$(window).resize(function() {
-			checkViewPortSize();
-			windowWidth = $(window).width();
-		});
-		return windowWidth;
-	}
+		//App init
+		getListSize();
+		checkViewPortSize();
+		controlsMouseMove();
+		assignMouseTouchEvents();
+		assignKeyboardEvents();
+		directionMouseMove();
+		initResize();
 
-	function checkViewPortSize() {
-		if (breakPointCheckMax.matches) {
-			//kill app
-			list.removeAttr('style');
-			killDirection();
-		} else {
-			//re-run app
-			setListSize();
-			runDirection();
-		}
-	}
+		//Only for testing
+		//clearAllIntervals();
 
-	//App init
-	getListSize();
-	checkViewPortSize();
-	controlsMouseMove();
-	assignMouseTouchEvents();
-	assignKeyboardEvents();
-	directionMouseMove();
-	initResize();
-
-	//Only for testing
-	//clearAllIntervals();
+		return true;
+	})();
 });
