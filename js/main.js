00fd05dea9d99d71dadef1723647c7c904edb53e
Richard Bultitude <rbultitude@thisiszone.com>
2015-05-15 17:44:26 +0100

    update documentation and pattern

--- js/main.js
+++ js/main.js
@@ -11,10 +11,9 @@
 
 $(document).ready(function() {
 
-	//var scrollingSlider = scrollingSlider || {};
-
 	(function(scrollingSlider) {
 		if (scrollingSlider !== undefined) {
+			console.log('An object with this name already exists');
 			return false;
 		}
 
